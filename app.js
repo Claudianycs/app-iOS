784adfd141e0db0c8005de79e661d1d898584086
Claudiany Sousa <claudiany2507@gmail.com>
2022-09-23 23:36:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+
+button.addEventListener('click', event => {
+    if (typeof currentStream !== 'undefined') {
+      stopMediaTracks(currentStream);
+    }
+    const videoConstraints = {};
+    if (select.value === '') {
+      videoConstraints.facingMode = 'environment';
+    } else {
+      videoConstraints.deviceId = { exact: select.value };
+    }
+    const constraints = {
+      video: videoConstraints,
+      audio: false
+    };
+  
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(stream => {
+        currentStream = stream;
+        video.srcObject = stream;
+        return navigator.mediaDevices.enumerateDevices();
+      })
+      .then(gotDevices)
+      .catch(error => {
+        console.error(error);
+      });
+  });
